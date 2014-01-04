fb1c430748b7f5eb08e4d7c624ff1350660ed8bd
Dezmin <contato@dezmin.com.br>
2014-01-04 03:40:20 -0200

    Refactoring

--- clirequire.js
+++ clirequire.js
@@ -17,13 +17,16 @@ Object.size = function(obj) {
 
 var require = function(name){
     var prefix = location.pathname
-    if (require.required.modules.indexOf(name) < 0){
-        $.getJSON(prefix + '/modules/'+name+'/package.json', function(data){
+    if ($.inArray(name,require.required.modules) < 0){
+    //if (require.required.modules.indexOf(name) < 0){
+        $.getJSON(prefix + '/modules/'+name+'/package.json')
+        .done(function(data){
             //Deal with dependent modules
             var modules = data.dependencies;
             if (Object.size(modules) > 0){
                 for (mod in modules){
-                    if (required.modules.indexOf(mod) < 0){
+                    if ($.inArray(mod,require.required.modules) < 0){
+                    //if (require.required.modules.indexOf(mod) < 0){
                         require(mod);
                         //Stop actual require and add module to stack
                         require.required.stack.push(name);
@@ -33,7 +36,7 @@ var require = function(name){
             }
             //Deal with dependent files
             var data_urls = [],
-                deps = data.main;
+            deps = data.main;
             if (typeof deps == 'string')
                 data_urls.push(deps)
             else if (typeof deps == 'object')
@@ -42,7 +45,8 @@ var require = function(name){
             var i = 0;
             for (el in data_urls){
                 var url = data_urls[el];
-                if (require.required.files.indexOf(url) < 0){
+                if ($.inArray(url,require.required.files) < 0){
+                //if (require.required.files.indexOf(url) < 0){
                     //Get proper file
                     $.get(prefix + '/modules/'+name+'/'+data_urls[el])
                     .done(function(data) {
@@ -66,9 +70,10 @@ var require = function(name){
                         require.required.files.push(url);
                         //Check if all files are included
                         i += 1;
+                        console.log(i + '/' + len + ' : ' + url)
                         if (i == len) {
                             $("body").trigger(name+"ready");
-                            console.log(name+" ready !");
+                            console.log(name+" ready!");
                             require.required.modules.push(name);
                             
                             //Since module is ready check if there were modules dependent on it
@@ -78,11 +83,16 @@ var require = function(name){
                                 
                         }
                     }
-                ).fail(function(data){console.log(data)});
+                    ).fail(function(data){
+                        console.log(data)
+                    });
                 } else {
                     console.log('File '+url+' already loaded!');
                 }//endif
             };
+        })
+        .fail(function(){
+            console.log('Module '+name+' not installed!');
         });    
     } else {
         console.log('Module '+name+' already loaded!');
@@ -99,10 +109,15 @@ require.required = {
     'stack': []
 }
 
-
-/* 
- * Actual bootstrap function
- */
-$(function(){
-    require('main')
-});
+//Add jquery
+var script = document.createElement("script");
+script.type = "text/javascript";
+script.src = './modules/main/js/jquery.js';
+script.onload = script.onreadystatechange = function(){
+    if (!script.readyState || (script.readyState === 'complete' || script.readyState === 'loaded')) {
+        script.onload = script.onreadystatechange = null
+        if($("#clirequire").attr('data-module'))
+            require($("#clirequire").attr('data-module'))
+    };
+}
+document.body.appendChild(script);
